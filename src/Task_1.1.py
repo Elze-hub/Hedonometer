37d2f07fe2f48851b4984e52008fe0a0a2732539
muulis <mmeelilepik@gmail.com>
2026-02-24 18:04:25 +0100

    Update: missing values replaced

--- src/Task_1.1.py
+++ src/Task_1.1.py
@@ -8,4 +8,13 @@ import pandas as pd
 ## Ignores first rows of text
 data = pd.read_csv('../data/raw/Data_Set_S1.txt', sep='\t', header = 2)
 
+## Convert numeric columns to numeric types (floats or integers)
+## This deemed to be necessary afterall, since some of the "--" values could not be identified when replacing
+numeric_columns = ['happiness_rank', 'happiness_average', 'happiness_standard_deviation', 'twitter_rank', 'google_rank', 'nyt_rank', 'lyrics_rank']
+for col in numeric_columns:
+    data[col] = pd.to_numeric(data[col], errors='coerce')
+
+
+## Replace missing values with NaN
+data = data.replace("--", pd.NA)
 print(data.head())
